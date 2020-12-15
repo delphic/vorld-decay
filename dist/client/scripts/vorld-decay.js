e94ce871d5c62ada0e1a88933fe89e7ccd1bd66a
delphic <delphic.uk@gmail.com>
2020-12-15 00:44:32 +0000

    updated fury to grab bug fix and remove demo weight now it has moved to another repo

--- dist/client/scripts/vorld-decay.js
+++ dist/client/scripts/vorld-decay.js
@@ -541,7 +541,7 @@ var Input = module.exports = function() {
 
 	var mouseDown = exports.mouseDown = function(button, thisFrame) {
 		if (!thisFrame) {
-			return mousePressed;
+			return mousePressed(button);
 		} else {
 			if (!isNaN(button) && !button.length) {
 				return downMouse[button];
